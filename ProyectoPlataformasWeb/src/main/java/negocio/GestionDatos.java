aa1fd85a3022caa7ef52475114c3109d5485e9fc
marcelo <marcelo_1993@hotmail.es>
2020-01-02 10:36:49 -0500

    creacion de generar citas medicas con insert y update y la pagina html

--- ProyectoPlataformasWeb/src/main/java/negocio/GestionDatos.java
+++ ProyectoPlataformasWeb/src/main/java/negocio/GestionDatos.java
@@ -6,16 +6,17 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import datos.CitaMedicaDAO;
 import datos.MedicoDAO;
 import datos.PacienteDAO;
-
+import modelo.CitaMedica;
 import modelo.Medico;
 //GestionAutorLocal
 import modelo.Paciente;
 
 
 @Stateless
-public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLocal,GestionMedicoLocal, GestionMedicoRemote {
+public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLocal,GestionMedicoLocal, GestionMedicoRemote,GestionCitaMedicaLocal, GestionCitaMedicaRemote {
 	
 	
 	@Inject
@@ -23,8 +24,11 @@ public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLoca
 	@Inject
 	private MedicoDAO daoMed;
 	
+	@Inject
+	private CitaMedicaDAO daoCiM;
 	
 	
+	//metodos para pacientes
 	
 	public void guardarPaciente(int codigo,String nombre, String apellido, String cedula, String direccion, String telefono,String email){
 		Paciente P = new Paciente();
@@ -47,6 +51,28 @@ public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLoca
 		return daoPr.getPacientesNombre(filtro);
 	}
 	
+	// metodos para citas medicas
+	public void guardarCitaMedica(int codigo,Paciente paciente, Medico medico, String fecha, String turno){
+		CitaMedica cm = new CitaMedica();
+		cm.setId(codigo);
+		cm.setPaciente(paciente);
+		cm.setMedico(medico);
+		cm.setFecha(fecha);
+		cm.setTurno(turno);
+		daoCiM.insert(cm);
+	}
+	
+	public List<CitaMedica> getCitasMedicas(){
+		return daoCiM.getCitasMedicas();
+	}
+	
+	public List<CitaMedica> getCitasMedicasNombre(String filtro)
+	{
+		return daoCiM.getCitasMedicasNombre(filtro);
+	}
+	
+	//metodos para medicos
+	
 	public void guardarMedico(int codigo,String nombre, String apellido, String cedula, String direccion, String telefono,String email, String area){
 		Medico m = new Medico(); 
 		m.setId(codigo);
@@ -58,6 +84,7 @@ public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLoca
 		m.setEmail(email);
 		m.setArea(area);
 		daoMed.insert(m);
+		
 		
 	}
 	
@@ -90,15 +117,31 @@ public class GestionDatos implements  GestionPacienteRemote ,GestionPacienteLoca
 		this.daoPr = daoPr;
 	}
 	
+	
+
+	public CitaMedicaDAO getDaoCiM() {
+		return daoCiM;
+	}
+
+	public void setDaoCiM(CitaMedicaDAO daoCiM) {
+		this.daoCiM = daoCiM;
+	}
 
 	@Override
 	public List<Paciente> getPaciente() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 
 	@Override
 	public List<Medico> getMedico() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	 @Override
+	public List<CitaMedica> getCitaMedica() {
 		// TODO Auto-generated method stub
 		return null;
 	}
